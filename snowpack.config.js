61eb16b0a94cd1b0b93f803ff7efaf9bc616733a
Nirmalya Ghosh <nirmalya.email@gmail.com>
2021-07-26 22:23:34 +0530

    feat: bootstraps an Astro project

---
+++ snowpack.config.js
@@ -0,0 +1,6 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  packageOptions: {
+    knownEntrypoints: ["astro/dist/internal/__astro_slot.js"],
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue,astro}"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  important: true,
+};
