d0c27142e0971af2cba80339d3411ad0e3d33148
Nirmalya Ghosh <nirmalya.email@gmail.com>
2021-07-27 16:13:49 +0530

    feat: adds MD styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,17 @@ export default {
   devOptions: {
     tailwindConfig: "./tailwind.config.js",
   },
+  markdownOptions: {
+    remarkPlugins: [
+      "remark-code-titles",
+      "remark-slug",
+      "remark-external-links",
+      "remark-toc",
+    ],
+    rehypePlugins: [
+      ["rehype-autolink-headings"],
+      [("rehype-toc", { headings: ["h2", "h3"] })],
+      ["rehype-add-classes", { "h1,h2,h3": "title" }],
+    ],
+  },
 };
